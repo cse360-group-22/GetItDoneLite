d1cbd4a9e03be7351f450e31737b7669543ae7d5
Vaughn Jacobson <jacobson.vaughn89@gmail.com>
2019-04-24 00:50:47 -0700

    Complete method along with error fixes and comments.

--- TeamProject360/src/proj/GUIFunctions.java
+++ TeamProject360/src/proj/GUIFunctions.java
@@ -79,14 +79,15 @@ public class GUIFunctions {
 		return myList;
 	}
 	
-	public static void SortDisplay(String selected)
+	public static void SortDisplay(ArrayList<Entry> sort, String selected)
 	{
+		//statements to sort each list type.
 		switch (selected)
 		{
 			case "Priority": 
-				Collections.sort(entries, new Comparator<Entry>() 
+				Collections.sort(sort, new Comparator<Entry>() 
 				{
-					public int compair(Entry e1, Entry e2)
+					public int compare(Entry e1, Entry e2)
 					{
 						return Integer.valueOf(e1.getPriority()).compareTo(e2.getPriority());
 					}
@@ -94,9 +95,9 @@ public class GUIFunctions {
 				break;
 				
 			case "Due Date":
-				Collections.sort(entries, new Comparator<Entry>()
+				Collections.sort(sort, new Comparator<Entry>()
 				{
-					public int compair(Entry e1, Entry e2)
+					public int compare(Entry e1, Entry e2)
 					{
 						return String.valueOf(e1.getDueDate()).compareTo(e2.getDueDate());
 					}
@@ -104,23 +105,43 @@ public class GUIFunctions {
 				break;
 				
 			case "Status":
-				Collections.sort(entries, new Comparator<Entry>()
+				Collections.sort(sort, new Comparator<Entry>()
 				{
-					public int compair(Entry e1, Entry e2)
+					public int compare(Entry e1, Entry e2)
 					{
 						return String.valueOf(e1.getStatus()).compareTo(e2.getStatus());
 					}
 				});
 				break;
+				
 			case "Description":
-				Collections.sort(entries, new Comparator<Entry>()
+				Collections.sort(sort, new Comparator<Entry>()
 				{
-					public int compair(Entry e1, Entry e2)
+					public int compare(Entry e1, Entry e2)
 					{
 						return String.valueOf(e1.getDescription()).compareTo(e2.getDescription());
 					}
 				});
 				break;
 		}
+	}
+	
+	public static ArrayList<Entry> Complete(ArrayList<Entry> myList, Entry completed, String date)
+	{
+		int index;
+		//empty list for completed tasks. This will need to be taken out of the function when implemented in the main.
+		ArrayList<Entry> completedList = new ArrayList<Entry>();
+		//add completed task to list
+		completedList.add(completedList.size(), completed);
+		SortDisplay(myList, "Priority");
+		//change the priority of all entries with a higher priority than the completed task to one less.
+		for(index = completed.getPriority() - 1 ; index < myList.size() ; index++)
+		{
+			myList.get(index + 1).setPriority(myList.get(index + 1).getPriority() - 1);
+		}
+		completed.finish(date);
+		//remove the completed task from the entry list.
+		myList.remove(completed.getPriority() - 1);	
+		return completedList;
 	}
 }
